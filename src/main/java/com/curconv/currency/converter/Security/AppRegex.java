d5e657922d1c1921299f8b7e25cf729285728cb3
MiIlenium <andrew.lavrinovich@gmail.com>
2024-11-22 16:42:50 +0100

    regex for country name case insensitivity

--- src/main/java/com/curconv/currency/converter/Security/AppRegex.java
+++ src/main/java/com/curconv/currency/converter/Security/AppRegex.java
@@ -4,5 +4,5 @@ public class AppRegex {
     //Match to country code
     public static final String THREE_DIGITS_COUNTRY_CODE = "^[A-Z]{3}$";
     public static final String TWO_DIGITS_COUNTRY_CODE = "^[A-Z]{2}$";
-    public static final String COUNTRY_NAME_CASE_INSENSITIVE = "/^[A-Z]*$/i";
+    public static final String COUNTRY_NAME_CASE_INSENSITIVE = "(?i)^[A-Z]*$";
 }
