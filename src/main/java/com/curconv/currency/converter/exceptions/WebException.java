6766da39d028095d67affd577de45883a4fee896
MiIlenium <andrew.lavrinovich@gmail.com>
2025-01-24 23:12:26 +0100

    Added WebException, improved exceptions in CurrencyProviderService, enabled dispatcher servlet, added actuator

---
+++ src/main/java/com/curconv/currency/converter/exceptions/WebException.java
@@ -0,0 +1,31 @@
+package com.curconv.currency.converter.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class WebException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    public WebException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public WebException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public WebException(Throwable cause, HttpStatus httpStatus) {
+        super(cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String toString() {
+        return httpStatus + ": " + super.toString();
+    }
+}
--- src/main/java/com/curconv/currency/converter/service/CurrencyProviderService.java
+++ src/main/java/com/curconv/currency/converter/service/CurrencyProviderService.java
@@ -2,6 +2,7 @@ package com.curconv.currency.converter.service;
 
 import com.curconv.currency.converter.dto.CurrencyDTO;
 import com.curconv.currency.converter.dto.wrapperDTO.CurrencyResponseDTO;
+import com.curconv.currency.converter.exceptions.WebException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,17 +25,20 @@ public class CurrencyProviderService {
     private String apiKey;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private static final String allCurrenciesEndpoint = "/v3/currencies";
-    private static final String getLatestAllCurrenciesEndpoint = "/v3/latest";
-    private static final String getByBaseCurrencyEndpoint = "/v3/latest?base_currency=";
+    private static final String ALL_CURRENCIES_ENDPOINT = "/v3/currencies";
+    private static final String GET_LATEST_ALL_CURRENCIES_ENDPOINT = "/v3/latest";
+    private static final String LATEST_BASE_CURRENCY = "/v3/latest?base_currency=";
+    private static final String ERROR_OCCURRED_WHILE_RETRIEVING_THE_DATA = "Error occurred while retrieving the data";
+    private static final String COULD_NOT_GET_DATA = "Could not get data from service: {}";
 
     public CurrencyDTO getCurrencyInfoByCountryCode(String countryCode) {
         CurrencyResponseDTO currencyResponseDTO;
         log.debug("Retrieving currency info for country code {}", countryCode);
         try {
-            currencyResponseDTO = objectMapper.readValue(sendRequest(allCurrenciesEndpoint, HttpMethod.GET), CurrencyResponseDTO.class);
+            currencyResponseDTO = objectMapper.readValue(sendRequest(ALL_CURRENCIES_ENDPOINT, HttpMethod.GET), CurrencyResponseDTO.class);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            log.error(COULD_NOT_GET_DATA, e);
+            throw new WebException(ERROR_OCCURRED_WHILE_RETRIEVING_THE_DATA, HttpStatus.BAD_REQUEST);
         }
 
         return currencyResponseDTO.getData().get(countryCode);
@@ -44,21 +48,23 @@ public class CurrencyProviderService {
         CurrencyResponseDTO currencyResponseDTO;
         log.debug("Retrieving latest exchange rate for country code {}", countryCode);
         try {
-            currencyResponseDTO = objectMapper.readValue(sendRequest(getLatestAllCurrenciesEndpoint, HttpMethod.GET), CurrencyResponseDTO.class);
+            currencyResponseDTO = objectMapper.readValue(sendRequest(GET_LATEST_ALL_CURRENCIES_ENDPOINT, HttpMethod.GET), CurrencyResponseDTO.class);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            log.error(COULD_NOT_GET_DATA, e);
+            throw new WebException(ERROR_OCCURRED_WHILE_RETRIEVING_THE_DATA, HttpStatus.BAD_REQUEST);
         }
 
         return currencyResponseDTO.getData().get(countryCode);
     }
 
-    public CurrencyDTO changeCurrency(String baseCurrency, String convertToCurrency, double amount){
+    public CurrencyDTO changeCurrency(String baseCurrency, String convertToCurrency, double amount) {
         CurrencyResponseDTO currencyResponseDTO;
 
         try {
-            currencyResponseDTO = objectMapper.readValue(sendRequest(getByBaseCurrencyEndpoint.concat(baseCurrency), HttpMethod.GET), CurrencyResponseDTO.class);
+            currencyResponseDTO = objectMapper.readValue(sendRequest(LATEST_BASE_CURRENCY.concat(baseCurrency), HttpMethod.GET), CurrencyResponseDTO.class);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            log.error(COULD_NOT_GET_DATA, e);
+            throw new WebException(ERROR_OCCURRED_WHILE_RETRIEVING_THE_DATA, HttpStatus.BAD_REQUEST);
         }
         //Get new object of currency and make calculations, return result
         CurrencyDTO currencyDTO = currencyResponseDTO.getData().get(convertToCurrency);
@@ -81,7 +87,7 @@ public class CurrencyProviderService {
             return restTemplate.exchange(baseUri, httpMethod, entity, String.class).getBody();
         } catch (Exception e) {
             log.error("Error during API call", e);
-            throw new RuntimeException("Error making API request", e);
+            throw new WebException("Error making API request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
